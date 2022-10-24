7d2c0ab1c8177641f1c3c153e3077e55633986fb
shominalexander <40673785+shominalexander@users.noreply.github.com>
2022-10-24 09:36:57 +0300

    Update delete.bat

--- delete.bat
+++ delete.bat
@@ -7,7 +7,9 @@ echo delete Cargo.lock file and target folder? ( y - yes, other - no )
 set /p delete=
 
 if [%delete%] == [y] (
- del Cargo.lock 
+ del Cargo.lock
+ 
  rmdir /s/q target
+ 
  pause
 )
